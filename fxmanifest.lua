3c41a01505a51b46d8e5f179dd2af6d5124cc16c
riversafe33 <riversafe33@outlook.com>
2025-06-07 12:11:40 +0200

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,7 +1,10 @@
 fx_version 'cerulean'
 game 'rdr3'
-
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'phonograph script '
+author 'riversafe'
+version '1.0.0'
 
 client_scripts {
     "@uiprompt/uiprompt.lua",
